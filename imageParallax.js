807fd68a1f1ed122ba2f961c9ca81d662053aa2b
Geo-M69 <128603618+Geo-M69@users.noreply.github.com>
2023-07-25 11:19:07 -0400

    Update imageParallax.js

--- imageParallax.js
+++ imageParallax.js
@@ -1,13 +1,10 @@
-var isPC = window.matchMedia("(min-width: 768px)").matches;
+// Parallax animation for background image //
 
-if (isPC) {
-  // Parallax animation for background image
-  document.addEventListener('scroll', function() {
+document.addEventListener('scroll', function() {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     var parallaxImage = document.querySelector('.background-image');
     parallaxImage.style.transform = 'translate3d(0, ' + scrollTop * 0.4 + 'px, 0)';
   });
-}
 
 // Services button fade-out animation //
 
