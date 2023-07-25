18a92c933fe5989ecd21c75f631562c8709c6edf
Geo-M69 <128603618+Geo-M69@users.noreply.github.com>
2023-07-25 09:54:31 -0400

    update

--- smoothScroll.js
+++ smoothScroll.js
@@ -26,3 +26,17 @@ document.addEventListener('DOMContentLoaded', function () {
       const distanceToServicesDiv = servicesDiv.getBoundingClientRect().top;
     });
   });
+
+  // Contact Smooth Scroll //
+document.addEventListener('DOMContentLoaded', function () {
+  // Get reference to the target div
+  const contactDiv = document.getElementById('contact-redirect');
+
+  // Add a click event listener to the anchor link
+  document.querySelector('a[href="#contactDiv"]').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent the default anchor link behavior
+
+    // Calculate the distance from the top of the page to the target div
+    const distanceToContactDiv = contactDiv.getBoundingClientRect().top;
+  });
+});
