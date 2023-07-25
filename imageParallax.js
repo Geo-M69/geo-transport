a3c08d8eca6e92b3b208d39087219e71c5b693d8
Geo-M69 <128603618+Geo-M69@users.noreply.github.com>
2023-07-25 11:54:31 -0400

    update

--- imageParallax.js
+++ imageParallax.js
@@ -1,12 +1,21 @@
-var isPC = window.matchMedia("(min-width: 768px)").matches;
-if (isPC) {
-  // Parallax animation for background image //
-  document.addEventListener('scroll', function() {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    var parallaxImage = document.querySelector('.background-image');
-    parallaxImage.style.transform = 'translate3d(0, ' + scrollTop * 0.4 + 'px, 0)';
-  });
+function parallaxScroll() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var parallaxImage = document.querySelector('.background-image');
+  parallaxImage.style.transform = 'translate3d(0, ' + scrollTop * 0.4 + 'px, 0)';
 }
+
+function isPCDevice() {
+  return window.matchMedia("(min-width: 768px)").matches;
+}
+
+if (isPCDevice()) {
+  var debouncedParallaxScroll = _.debounce(parallaxScroll, 10);
+  window.addEventListener('scroll', debouncedParallaxScroll);
+} else {
+  var parallaxImage = document.querySelector('.background-image');
+  parallaxImage.style.transform = 'none';
+}
+
 
 // Services button fade-out animation //
 
